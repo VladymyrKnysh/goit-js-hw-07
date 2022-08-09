51c55f91fb892209582d9280034aea85611f508e
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-09 11:09:03 +0300

    ok

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+ const markup = createMarkup(galleryItems)
+
+function createMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+   return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  })
+    .join('')
+    console.log(markup);
+}
+const container = document.querySelector('.gallery')
+container.insertAdjacentHTML('afterbegin', markup)
+    
+
+
+
